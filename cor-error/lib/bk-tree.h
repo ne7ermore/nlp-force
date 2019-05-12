433063b34b89b6f952da86dbfb49e9c8d462d22d
Tao <422618856@qq.com>
2019-05-12 21:19:25 +0800

    complete utils head

--- cor-error/lib/bk-tree.h
+++ cor-error/lib/bk-tree.h
@@ -1,7 +1,8 @@
+#pragma once
+
 #include <string>
 #include <unordermap>
 #include <vector>
-
 
 namespace Bkt {
     struct Node {
--- cor-error/lib/utils.h
+++ cor-error/lib/utils.h
@@ -1,10 +1,27 @@
+#pragma once
+
 #include <string>
-#include <unordermap>
 #include <vector>
+#include <sys/time.h>
 
 namespace Bkt {
     namespace utils {
-        int edit_distance(std::string, std::string);
+        class time {
+            public:
+                print_time () { gettimeofday(&start, NULL); }
+                void refresh();
+                void print();
+            private:
+                struct timeval start, end;
+        }
+
+        int levenshtein_distance(std::wstring, std::wstring);
+
         std::wstring str2wstr(std::string);
-    }
-}
+
+        bool pair_sort(const std::);
+        bool is_digits(const std::string&);
+
+        float std_dev(const std::vector<float>&);
+    } // namespace utils
+} // namespace Bkt
