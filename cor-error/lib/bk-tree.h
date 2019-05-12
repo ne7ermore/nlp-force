e5d8bfce70b2ce4ed7bc03f7818ef3560d890da9
Tao <422618856@qq.com>
2019-05-12 21:02:11 +0800

    complete .h

--- cor-error/lib/bk-tree.h
+++ cor-error/lib/bk-tree.h
@@ -6,20 +6,26 @@
 namespace Bkt {
     struct Node {
         Node() : value() {};
-        Node(std::string in) : value(in) {};
-
+        Node(std::string wstr, float score) : value(wstr), score(score) {};
+        float score;
         std::string value;
         std::unordermap<int, Node *> leafs;
     }
 
     class Tree {
         public:
-            Tree() : root() {};
-            ~Tree();
+            Tree() : root() { root = new Node; };
             void add(std::string);
-            std::vector search(std::string, int);
+            std::pair<std::wstring, float> get(std::string, int);
             int fn(std::string, std::string);
+            Node* root();
+            void read_file(std::string);
+
+            ~Tree();
+            void destroy(Node *)
+
         private:
+            float pow_num = 0.5, min_score = 100;
             Node * root;
     }
 } //namespace Bkt
